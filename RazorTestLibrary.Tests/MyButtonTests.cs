ecf2d9761b0a7fb5bde0d090f236f6688a7f2ce2
Erich Thalacker <egthalacker@gmail.com>
2021-02-09 08:45:45 -0600

    Add project files.

---
+++ RazorTestLibrary.Tests/MyButtonTests.cs
@@ -0,0 +1,31 @@
+﻿using Bunit;
+using Bunit.JSInterop;
+using Microsoft.AspNetCore.Components.Web;
+using Xunit;
+
+namespace RazorTestLibrary.Tests
+{
+    public class MyButtonTests : TestContext
+    {
+        static IRenderedComponent<MyButton> component;
+        
+        public MyButtonTests()
+        {
+            component = SetupButtonComponent();
+        }
+
+        private IRenderedComponent<MyButton> SetupButtonComponent()
+        {
+            var jsModule = JSInterop.SetupModule("import", new string[] { "./_content/HeadlessUI/common.js" });
+            jsModule.SetupVoid().SetVoidResult();
+            return RenderComponent<MyButton>();
+        }
+
+        [Fact]
+        public void NewButton_HasButtonTag()
+        {
+            var button = component.Find("button");
+            Assert.NotNull(button);
+        }        
+    }
+}
---
+++ RazorTestLibrary/wwwroot/common.js
@@ -0,0 +1,10 @@
+﻿export function preventDefaultKeyBehaviorOnKeys(arrayOfKeyStrings) {    
+    var preventDefaultOnKeysFunction = function (e) {        
+        if (arrayOfKeyStrings.includes(e.key)) {
+            e.preventDefault();
+            return false;
+        }
+    }
+    window.addEventListener('keydown', preventDefaultOnKeysFunction, false);
+    window.addEventListener('keyup', preventDefaultOnKeysFunction, false);    
+}
